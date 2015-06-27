4d6ce93fa4c76b6459aa31f23c5d3b229ffa1619
Rob Fielding <rob.fielding@gmail.com>
2015-06-27 12:12:11 -0400

    pass bytes that we dont understand

--- octaveRounder.ino
+++ octaveRounder.ino
@@ -27,105 +27,106 @@ int toggle = 1;
 
 void setup() {
   Serial.begin(31250);
-  pinMode(13,OUTPUT);
+  pinMode(13, OUTPUT);
   //unnecessary
-  pinMode(2,OUTPUT);
+  pinMode(2, OUTPUT);
   //necessary?
-  pinMode(1,OUTPUT);
+  pinMode(1, OUTPUT);
   //necessary?
-  pinMode(0,INPUT);
+  pinMode(0, INPUT);
   //unnecessary
-  digitalWrite(2,HIGH);
+  digitalWrite(2, HIGH);
   midi_state = midi_needStatus;
-  for(int i=0; i < midi_noteCount; i++) {
+  for (int i = 0; i < midi_noteCount; i++) {
     shifted_noteDown[i] = i;
   }
 }
 
 void doFilterOnOff() {
   int diff = 0;
-            //arg1_byte == 0 for 0x90 is similar to 0x80
-            if(cmd_byte == 0x90 && arg1_byte != 0) {
-              //If there was a previous note down, then compare and possibly shift....
-              if(last_noteDown != never_noteDown) {
-                diff = (int)arg2_byte - (int)last_noteDown;
-                if(diff > 6 && ((int)arg2_byte + (octave_shift-1)*12) >= 0) {
-                  octave_shift--;
-                }
-                if(diff < -6 && ((int)arg2_byte + (octave_shift+1)*12) < midi_noteCount) {
-                  octave_shift++;
-                }
-              }
-              //Record how the note went down, and use that to plug in the note as it comes back up.
-              //We can only handle 1 channel and one note down per note!!!
-              shifted_noteDown[arg2_byte] = (((int)arg2_byte) + octave_shift * 12);
-              shifted_noteDown[arg2_byte] %= midi_noteCount; //This is the note that we translate to on note up
-              last_noteDown = arg2_byte; //can no longer be never_noteDown
-              arg2_byte = shifted_noteDown[arg2_byte]; //This is the byte we send (octave translated).
-            }
-            if(cmd_byte == 0x80 || arg1_byte == 0) {
-              arg2_byte = shifted_noteDown[arg2_byte];
-            }
+  //arg1_byte == 0 for 0x90 is similar to 0x80
+  if (cmd_byte == 0x90 && arg1_byte != 0) {
+    //If there was a previous note down, then compare and possibly shift....
+    if (last_noteDown != never_noteDown) {
+      diff = (int)arg2_byte - (int)last_noteDown;
+      if (diff > 6 && ((int)arg2_byte + (octave_shift - 1) * 12) >= 0) {
+        octave_shift--;
+      }
+      if (diff < -6 && ((int)arg2_byte + (octave_shift + 1) * 12) < midi_noteCount) {
+        octave_shift++;
+      }
+    }
+    //Record how the note went down, and use that to plug in the note as it comes back up.
+    //We can only handle 1 channel and one note down per note!!!
+    shifted_noteDown[arg2_byte] = (((int)arg2_byte) + octave_shift * 12);
+    shifted_noteDown[arg2_byte] %= midi_noteCount; //This is the note that we translate to on note up
+    last_noteDown = arg2_byte; //can no longer be never_noteDown
+    arg2_byte = shifted_noteDown[arg2_byte]; //This is the byte we send (octave translated).
+  }
+  if (cmd_byte == 0x80 || arg1_byte == 0) {
+    arg2_byte = shifted_noteDown[arg2_byte];
+  }
 }
 
 //When we are in send state
 void doSend() {
-  if(midi_state == midi_send) {
-        switch(cmd_byte) {
-          case 0x90:
-          case 0x80:
-            doFilterOnOff();
-          case 0xA0:
-          case 0xB0:
-          case 0xC0:
-          case 0xE0:
-            Serial.write(status_byte);
-            Serial.write(arg2_byte);
-            Serial.write(arg1_byte);
-            break;
-          case 0xD0:
-            Serial.write(status_byte);
-            Serial.write(arg1_byte);
-            break;
-          default:
-            break;
-        }
-        midi_state = midi_needStatus;
+  if (midi_state == midi_send) {
+    switch (cmd_byte) {
+      case 0x90:
+      case 0x80:
+        doFilterOnOff();
+      case 0xA0:
+      case 0xB0:
+      case 0xC0:
+      case 0xE0:
+        Serial.write(status_byte);
+        Serial.write(arg2_byte);
+        Serial.write(arg1_byte);
+        break;
+      case 0xD0:
+        Serial.write(status_byte);
+        Serial.write(arg1_byte);
+        break;
+      default:
+        break;
+    }
+    midi_state = midi_needStatus;
   }
 }
 
 //Get a status byte and set the state depending on it
 void needStatus() {
-  if(midi_state == midi_needStatus) {
-      if(Serial.available() > 0) {
-        status_byte = Serial.read();
-        cmd_byte = status_byte & 0xF0;
-        chan_byte = status_byte & 0x0F;
-        switch(cmd_byte) {
-          case 0x90:
-          case 0x80:
-            digitalWrite(13,toggle%2);
-            toggle++;
-          case 0xA0:
-          case 0xB0:
-          case 0xC0:
-          case 0xE0:
-            midi_state = midi_2Args;
-            break;
-          case 0xD0:
-            midi_state = midi_1Args;
-            break;
-          default:
-            break;
-        }
+  if (midi_state == midi_needStatus) {
+    if (Serial.available() > 0) {
+      status_byte = Serial.read();
+      cmd_byte = status_byte & 0xF0;
+      chan_byte = status_byte & 0x0F;
+      switch (cmd_byte) {
+        case 0x90:
+        case 0x80:
+          digitalWrite(13, toggle % 2);
+          toggle++;
+        case 0xA0:
+        case 0xB0:
+        case 0xC0:
+        case 0xE0:
+          midi_state = midi_2Args;
+          break;
+        case 0xD0:
+          midi_state = midi_1Args;
+          break;
+        default:
+          Serial.write(status_byte); //No idea what it is
+          break;
       }
+    }
   }
 }
 
 //Get second to last arg before send
 void need2Args() {
-  if(midi_state == midi_2Args) {
-    if(Serial.available() > 0) {
+  if (midi_state == midi_2Args) {
+    if (Serial.available() > 0) {
       arg2_byte = Serial.read();
       midi_state = midi_1Args;
     }
@@ -134,8 +135,8 @@ void need2Args() {
 
 //Get the last arg before we must send
 void need1Args() {
-  if(midi_state == midi_1Args) {
-    if(Serial.available() > 0) {
+  if (midi_state == midi_1Args) {
+    if (Serial.available() > 0) {
       arg1_byte = Serial.read();
       midi_state = midi_send;
     }
