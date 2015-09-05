ce0e2202b23d3996f71f6a462f92d1396dbcb1c1
Rob Fielding <rob.fielding@gmail.com>
2015-09-05 02:23:49 -0400

    add in running status for everything

--- octaveRounder.ino
+++ octaveRounder.ino
@@ -7,6 +7,7 @@ const int split_point = 60;
 const int oct_notes = 12;
 const int ceiling7bit = 128;
 
+byte running_status = 0;
 byte cmd_channel = 0;
 byte cmd_state = 0;
 byte cmd_args[2];
@@ -18,7 +19,7 @@ byte rpn_lsb = 0x7F;
 byte rpn_msb = 0x7F;
 byte rpn_msb_data = 0;
 byte rpn_lsb_data = 0;
- 
+
 int pitch_wheel_in = pitch_wheel_centered;
 int pitch_wheel_sent = pitch_wheel_centered;
 int pitch_wheel_adjust = 0;
@@ -52,7 +53,7 @@ void setup() {
   rpn_msb = 0x7F;
   rpn_msb_data = 0;
   rpn_lsb_data = 0;
-  for(int i=0; i<midi_note_count; i++) {
+  for (int i = 0; i < midi_note_count; i++) {
     notes[i].id = 0;
     notes[i].sent_note = 0;
     notes[i].sent_vol = 0;
@@ -61,7 +62,7 @@ void setup() {
 }
 
 static int cmd_arg_count(const byte c) {
-  switch(c) {
+  switch (c) {
     case 0x90: //note on
     case 0x80: //note off
     case 0xA0: //aftertouch
@@ -76,24 +77,33 @@ static int cmd_arg_count(const byte c) {
   }
 }
 
-static void forward_xmit() {
+static void status_xmit(byte cmd, byte channel) {
   //TODO: if we get program change, or pitch bend sensitivity, we should have responded
-  Serial.write( cmd_state | cmd_channel );
-  for(int i=0; i<cmd_has; i++) {
+  if(cmd == 0xB0 && running_status == cmd) {
+    //Do running status on these messages, because it's very common to expect it.
+  } else {
+    Serial.write( cmd | channel );
+  }
+  running_status = cmd;
+}
+
+static void forward_xmit() {
+  status_xmit(cmd_state, cmd_channel);
+  for (int i = 0; i < cmd_has; i++) {
     Serial.write( cmd_args[i] );
   }
 }
 
 static void pitch_wheel_xmit() {
   int adjusted = pitch_wheel_in + pitch_wheel_adjust;
-  if( adjusted < 0 ) {
+  if ( adjusted < 0 ) {
     adjusted = 0;
   }
-  if( adjusted+1 >= pitch_wheel_centered*2 ) {
-    adjusted = pitch_wheel_centered*2 - 1;
+  if ( adjusted + 1 >= pitch_wheel_centered * 2 ) {
+    adjusted = pitch_wheel_centered * 2 - 1;
   }
-  if( adjusted != pitch_wheel_sent ) {
-    Serial.write( 0xE0 | cmd_channel );
+  if ( adjusted != pitch_wheel_sent ) {
+    status_xmit(0xE0, cmd_channel);
     Serial.write( adjusted % ceiling7bit );
     Serial.write( adjusted / ceiling7bit );
     pitch_wheel_sent = adjusted;
@@ -101,13 +111,13 @@ static void pitch_wheel_xmit() {
 }
 
 static void note_message_re_xmit(const int n, const int v) {
-  Serial.write( cmd_state | cmd_channel );
+  status_xmit(cmd_state, cmd_channel);
   Serial.write( n );
   Serial.write( v );
 }
 
 static void note_message_xmit() {
-  Serial.write( cmd_state | cmd_channel );
+  status_xmit(cmd_state, cmd_channel);
   Serial.write( notes[cmd_args[0]].sent_note );
   Serial.write( notes[cmd_args[0]].sent_vol );
 }
@@ -118,78 +128,78 @@ static int in_quartertone_zone(const byte n) {
 
 static void quartertone_adjust(const byte n) {
   pitch_wheel_adjust = 0;
-  if(in_quartertone_zone(n)) {
-    pitch_wheel_adjust -= (pitch_wheel_centered/(2*pitch_wheel_semis));
+  if (in_quartertone_zone(n)) {
+    pitch_wheel_adjust -= (pitch_wheel_centered / (2 * pitch_wheel_semis));
   }
 }
 
 static byte oct_rounding() {
-    int nSend = cmd_args[0]+note_adjust;
-    //Might need a switch so that fullJump is always true.  
-    const int fullJump = (in_quartertone_zone(cmd_last) != in_quartertone_zone(cmd_args[0]));
-    //TODO: can all of the while loops be dispensed with for a constant-time calculation?
-    if(cmd_last != cmd_last_never) {
-      int diff = (nSend - note_adjust) - cmd_last;
-      if(diff > oct_notes/2 && nSend >= oct_notes) {
-        do {
-          nSend -= oct_notes;
-          note_adjust -= oct_notes;
-          diff -= oct_notes;
-        }while(diff > oct_notes/2 && nSend >= oct_notes && fullJump);
-      } 
-      if(diff < -oct_notes/2 && nSend < (midi_note_count-oct_notes)) {
-        do {
-          nSend += oct_notes;
-          note_adjust += oct_notes;
-          diff += oct_notes;
-        }while(diff < -oct_notes/2 && nSend < (midi_note_count-oct_notes) && fullJump);
-      } 
+  int nSend = cmd_args[0] + note_adjust;
+  //Might need a switch so that fullJump is always true.
+  const int fullJump = (in_quartertone_zone(cmd_last) != in_quartertone_zone(cmd_args[0]));
+  //TODO: can all of the while loops be dispensed with for a constant-time calculation?
+  if (cmd_last != cmd_last_never) {
+    int diff = (nSend - note_adjust) - cmd_last;
+    if (diff > oct_notes / 2 && nSend >= oct_notes) {
+      do {
+        nSend -= oct_notes;
+        note_adjust -= oct_notes;
+        diff -= oct_notes;
+      } while (diff > oct_notes / 2 && nSend >= oct_notes && fullJump);
     }
-    while(nSend < 0) {
-      nSend += oct_notes;
-      note_adjust += oct_notes;
+    if (diff < -oct_notes / 2 && nSend < (midi_note_count - oct_notes)) {
+      do {
+        nSend += oct_notes;
+        note_adjust += oct_notes;
+        diff += oct_notes;
+      } while (diff < -oct_notes / 2 && nSend < (midi_note_count - oct_notes) && fullJump);
     }
-    while(nSend >= midi_note_count) {
-      nSend -= oct_notes;
-      note_adjust -= oct_notes;
-    }
-    return (byte)nSend;
+  }
+  while (nSend < 0) {
+    nSend += oct_notes;
+    note_adjust += oct_notes;
+  }
+  while (nSend >= midi_note_count) {
+    nSend -= oct_notes;
+    note_adjust -= oct_notes;
+  }
+  return (byte)nSend;
 }
 
 static void find_leader(byte* ptr_lead_idx, int* ptr_lead_same) {
-    const byte n = cmd_args[0];
-    int lead_id = 0;
-    int notes_on = 0;
-    for(byte i=0; i<midi_note_count; i++) {
-      if( notes[i].sent_vol > 0 ) {
-        notes_on++;
-        if( lead_id <= notes[i].id ) {
-          lead_id  = notes[i].id;
-          *ptr_lead_idx = i;
-        }
+  const byte n = cmd_args[0];
+  int lead_id = 0;
+  int notes_on = 0;
+  for (byte i = 0; i < midi_note_count; i++) {
+    if ( notes[i].sent_vol > 0 ) {
+      notes_on++;
+      if ( lead_id <= notes[i].id ) {
+        lead_id  = notes[i].id;
+        *ptr_lead_idx = i;
       }
     }
-    if(notes_on == 0) {
-      for(byte i=0; i<midi_note_count; i++) {
-        notes[i].id = 0;
-        cmd_id = 0;
-      }
+  }
+  if (notes_on == 0) {
+    for (byte i = 0; i < midi_note_count; i++) {
+      notes[i].id = 0;
+      cmd_id = 0;
     }
-    *ptr_lead_same = ((*ptr_lead_idx != n) && (notes[n].sent_note == notes[*ptr_lead_idx].sent_note));
+  }
+  *ptr_lead_same = ((*ptr_lead_idx != n) && (notes[n].sent_note == notes[*ptr_lead_idx].sent_note));
 }
 
 static void note_turnoff() {
-      Serial.write( cmd_state | cmd_channel );
-      Serial.write( notes[cmd_args[0]].sent_note );
-      Serial.write( 0 );
+  status_xmit(cmd_state, cmd_channel);
+  Serial.write( notes[cmd_args[0]].sent_note );
+  Serial.write( 0 );
 }
 
 static int count_duplicates() {
   int count = 0;
   const byte n = cmd_args[0];
   const byte nSend = notes[n].sent_note;
-  for(byte i=0; i<midi_note_count; i++) {
-    if(notes[i].sent_note == nSend && notes[i].sent_vol > 0) {
+  for (byte i = 0; i < midi_note_count; i++) {
+    if (notes[i].sent_note == nSend && notes[i].sent_vol > 0) {
       count++;
     }
   }
@@ -201,7 +211,7 @@ static void note_message() {
   const byte v = cmd_args[1];
   byte lead_idx = n;
   int lead_same = 0;
-  if(v > 0) {
+  if (v > 0) {
     const byte nSend = oct_rounding();
     find_leader(&lead_idx, &lead_same);
     cmd_id++;
@@ -209,10 +219,10 @@ static void note_message() {
     notes[n].sent_note = nSend; //!overwritten so that lead_same is different when recomputed now!
     notes[n].sent_vol = cmd_args[1];
     const int count = count_duplicates();
-    if(lead_same || count > 1) {
+    if (lead_same || count > 1) {
       note_turnoff(); //Done so that total on and off for a note always end up as 0
-    } 
-    if(n==lead_idx || notes[n].sent_note != notes[lead_idx].sent_note) {
+    }
+    if (n == lead_idx || notes[n].sent_note != notes[lead_idx].sent_note) {
       quartertone_adjust(n);
       pitch_wheel_xmit();
     }
@@ -228,30 +238,30 @@ static void note_message() {
     quartertone_adjust(lead_idx);
     pitch_wheel_xmit();
     //If we just unburied the same note, then turn it back on (midi mono won't but should)
-    if(lead_same) {
-      note_message_re_xmit(notes[n].sent_note, old_vol); 
+    if (lead_same) {
+      note_message_re_xmit(notes[n].sent_note, old_vol);
     }
   }
 }
 
 //As relevant control info passes through, sync with it if we have to
 static void handle_controls() {
-  if( cmd_args[0] == 0x64 ) {
+  if ( cmd_args[0] == 0x64 ) {
     rpn_lsb = cmd_args[1];
   }
-  if( cmd_args[0] == 0x65 ) {
+  if ( cmd_args[0] == 0x65 ) {
     rpn_msb = cmd_args[1];
-  }  
-  if( cmd_args[0] == 0x06 ) {
+  }
+  if ( cmd_args[0] == 0x06 ) {
     rpn_msb_data = cmd_args[1];
-    if( rpn_msb == 0 ) {
+    if ( rpn_msb == 0 ) {
       //TODO: we assume that the pitch wheel is centered when we get this message
       //pitch_wheel_semis = rpn_msb_data;
     }
   }
-  if( cmd_args[0] == 0x26 ) {
+  if ( cmd_args[0] == 0x26 ) {
     rpn_lsb_data = cmd_args[1];
-    if( rpn_lsb == 0 ) {
+    if ( rpn_lsb == 0 ) {
       //TODO: not even going to bother with cents until semis work
       //pitch_wheel_cents = rpn_lsb_data;
     }
@@ -259,7 +269,7 @@ static void handle_controls() {
 }
 
 static void handle_message() {
-  switch(cmd_state) {
+  switch (cmd_state) {
     case 0x80:
       cmd_state = 0x90;
       cmd_args[1] = 0x00;
@@ -267,45 +277,45 @@ static void handle_message() {
       note_message();
       break;
     case 0xE0:
-      pitch_wheel_in = cmd_args[0] + ceiling7bit*cmd_args[1];
+      pitch_wheel_in = cmd_args[0] + ceiling7bit * cmd_args[1];
       pitch_wheel_xmit();
       break;
     case 0xB0:
       handle_controls();
       forward_xmit();
-      break;  
+      break;
     default:
       forward_xmit();
-      break;  
+      break;
   }
 }
 
 static void byte_enqueue(byte b) {
   //Pass 0xF0 messages literally and immediately.
-  if((b & 0xF0) == 0xF0) {
+  if ((b & 0xF0) == 0xF0) {
     cmd_state = 0xF0;
     Serial.write(b);
-    if(b == 0xFF) {
+    if (b == 0xFF) {
       setup(); //Let's reset if we see a MIDI reset going by.
     }
     return;
   }
-  if((b & 0x80)==0 && cmd_state == 0xF0) {
+  if ((b & 0x80) == 0 && cmd_state == 0xF0) {
     Serial.write(b);
     return;
   }
 
-  if(b & 0x80) {
+  if (b & 0x80) {
     cmd_state   = (b & 0xF0);
     cmd_channel = (b & 0x0F);
-    cmd_needs = cmd_arg_count(cmd_state); 
+    cmd_needs = cmd_arg_count(cmd_state);
     cmd_has = 0;
   } else {
-    if( cmd_needs > 0 ) {
+    if ( cmd_needs > 0 ) {
       cmd_args[ cmd_has ] = b;
       cmd_has++;
     }
-    if( cmd_has == cmd_needs ) {
+    if ( cmd_has == cmd_needs ) {
       handle_message();
       cmd_needs = cmd_arg_count(cmd_state);
       cmd_has = 0;
@@ -315,7 +325,7 @@ static void byte_enqueue(byte b) {
 
 //No calls to available or read should happen elsewhere
 void loop() {
-  while(Serial.available()) {
+  while (Serial.available()) {
     byte_enqueue(Serial.read());
   }
 }
