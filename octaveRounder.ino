518eb5a93757446255a87a6f8d6afea2700c4164
Rob Fielding <rob.fielding@gmail.com>
2015-11-18 00:47:51 -0500

    basic echo working

--- octaveRounder.ino
+++ octaveRounder.ino
@@ -1,9 +1,11 @@
 const int midi_serial_rate = 31250;
 const int midi_note_count = 128;
 const int ring_buffer_size = 512;
-int midi_time=0;
-int spill_time=0;
-
+int midi_time=-1;
+int spill_time=-1;
+bool delay_real_time=false;
+byte echo_status_byte = 0;
+int echo_status_parm = 0;
 int ring_buffer_head = 0;
 int ring_buffer_tail = 0;
 byte ring_buffer[ring_buffer_size];
@@ -34,25 +36,45 @@ static bool ring_buffer_empty() {
 }
 
 static bool isDueWithin(int limit) {
-  return (midi_time-spill_time) > 0;
+  return (midi_time-spill_time) >= limit;
 }
 
 static void byte_buffer(byte bin) {
   //First, spill everything that is due for spilling, which (should) guarantee space to write data.
-  while(!ring_buffer_empty() && isDueWithin(24)) {
-    byte bout = ring_buffer[ring_buffer_head];
+  while(!ring_buffer_empty() && isDueWithin(16)) {
+    byte bout = ring_buffer[ring_buffer_tail];
     if(bout == 0xF8) {
       spill_time++;
     }
-    Serial.write(bout);
-    ring_buffer_head++;
-    ring_buffer_head %= ring_buffer_size;  
+    if(bout & 0x80) {
+      echo_status_byte = bout;
+      echo_status_parm = 0;
+    } else {
+      echo_status_parm++;
+    }
+    if(bout & 0x80) {
+      //We need to not propagate real-time messages a second time.
+      //Watch for data bytes of real-time messages
+      delay_real_time = ((bout & 0xF0)==0xF0);
+    }
+    if(!delay_real_time) {
+      if( (echo_status_byte & 0xF0)==0x90 && echo_status_parm==1 ) {
+        bout = (bout + 12 + 128) % 128;
+      }
+      if( (echo_status_byte & 0xF0)==0x90 && echo_status_parm==2 ) {
+        bout = (bout * 3) / 4;
+      }
+      if( (echo_status_byte & 0xF0)==0x80 && echo_status_parm==1 ) {
+        bout = (bout + 12 + 128) % 128;
+      }
+      Serial.write(bout);
+    }
+    ring_buffer_tail++;
+    ring_buffer_tail %= ring_buffer_size;  
   }
-  //Assume ring buffer is not full: 
-  // (ring_buffer_tail+1)%ring_buffer_size != ring_buffer_head
-  ring_buffer_tail++;
-  ring_buffer_tail %= ring_buffer_size;
   ring_buffer[ring_buffer_head] = bin;
+  ring_buffer_head++;
+  ring_buffer_head %= ring_buffer_size;
 }
 
 
@@ -61,7 +83,8 @@ void byte_enqueue(byte b) {
     showTime(b);
     midi_time++;
   }
-  
+
+  Serial.write(b);
   byte_buffer(b);  
 }
 
